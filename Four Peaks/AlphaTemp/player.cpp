bef4a97594ff0eb3dc8b94401fd8c91e4681889d
justin <c.justintsekang@digipen.edu>
2026-01-31 23:43:13 +0800

    test

--- Four Peaks/AlphaTemp/player.cpp
+++ Four Peaks/AlphaTemp/player.cpp
@@ -219,8 +219,8 @@ void PlayerUpdate(Player& p, float dt)
 
             case Player::GroundType::Normal:
             case Player::GroundType::Spikes:
-                accel = 10.0f;
-                decel = 8.0f;
+                accel = 20.0f;
+                decel = 4.0f;
                 break;
             
             //! can change to "Spikes" to see the effect first
