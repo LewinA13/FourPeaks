a0e50e65c41cd7fe0bda76074e3b1f3661c03e67
Skyler <skylerling27@gmail.com>
2026-04-05 15:34:54 +0800

    Made More Changes

--- Four Peaks/AlphaTemp/include/scenes/pause.hpp
+++ Four Peaks/AlphaTemp/include/scenes/pause.hpp
@@ -2,6 +2,20 @@
 // Done By: Justin
 // ----------------------------------------------------------------------------
 
+//---------------------------------------------------------------------------
+// Copyright (C) 2026 Team Game++ (Four Peaks)
+// All rights reserved.
+//
+// This file is part of the Four Peaks project. All code, design, and original
+// assets are the work of LewinA and team members unless otherwise stated.
+//
+// Audio assets are sourced from Soundly and used under appropriate licensing.
+//
+// Reproduction, distribution, or modification of this file or its contents,
+// in whole or in part, without prior written permission is strictly prohibited.
+//
+//---------------------------------------------------------------------------
+
 #ifndef PAUSE_HPP
 #define PAUSE_HPP
 
--- Four Peaks/AlphaTemp/src/scenes/spring.cpp
+++ Four Peaks/AlphaTemp/src/scenes/spring.cpp
@@ -2,6 +2,20 @@
 // Done By: Hong Yang, Skyler, Arun, Justin
 // ----------------------------------------------------------------------------
 
+//---------------------------------------------------------------------------
+// Copyright (C) 2026 Team Game++ (Four Peaks)
+// All rights reserved.
+//
+// This file is part of the Four Peaks project. All code, design, and original
+// assets are the work of LewinA and team members unless otherwise stated.
+//
+// Audio assets are sourced from Soundly and used under appropriate licensing.
+//
+// Reproduction, distribution, or modification of this file or its contents,
+// in whole or in part, without prior written permission is strictly prohibited.
+//
+//---------------------------------------------------------------------------
+
 // ---------------------------------------------------------------------------
 //spring.cpp
 // This file contains the implementation of the spring stage, including the dynamic wind system and its visual effects.
